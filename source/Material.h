1d6804a174b6529b00ac13ad4525c02407e0a635
Qionn <wullaert.liam@outlook.com>
2023-10-06 23:08:03 +0200

    implemented final scene

--- source/Material.h
+++ source/Material.h
@@ -106,8 +106,6 @@ namespace dae
 
 		ColorRGB Shade(const HitRecord& hitRecord = {}, const Vector3& l = {}, const Vector3& v = {}) override
 		{
-			//todo: W3
-			assert(false && "Not Implemented Yet");
 			return {};
 		}
 
